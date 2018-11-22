d9dd186e2a67811e0b1cc718edc94d4c23a5ed8a
Sergey <sergey.batalev@yandex.ru>
2018-11-22 21:30:57 +0300

    small refactor

--- config.go
+++ config.go
@@ -2,32 +2,32 @@ package main
 
 import (
 	"encoding/json"
+	"fmt"
 	"io/ioutil"
-    "fmt"
 	"os"
 )
 
 // Config structure
 type Config struct {
-	Temperature int    `json:"max_temperature"`
-	Email       string `json:"email"`
-	Password    string `json:"password"`
-	UpdateTime  int    `json:"update_time"`
-	HTMLURL1    string `json:"html_url_1"`
-	HTMLURL2    string `json:"html_url_2"`
+	Temperature int
+	Email       string
+	Password    string
+	UpdateTime  int
+	HTMLURL1    string
+	HTMLURL2    string
 }
 
 func getConfig() *Config {
 	file, err := ioutil.ReadFile("data/config.json")
 	if err != nil {
-        fmt.Println(err)
+		fmt.Println(err)
 		os.Exit(0)
 	}
 
 	config := &Config{}
 	err = json.Unmarshal(file, config)
 	if err != nil {
-        fmt.Println("kek2")
+		fmt.Println("kek2")
 		os.Exit(0)
 	}
 
