66525a3fcb3cd8084187f2f0e3944969df06daf8
Sergey <sergey.batalev@yandex.ru>
2018-12-01 18:33:46 +0300

    add notifier service

---
+++ notify.go
@@ -0,0 +1,27 @@
+package main
+
+import "log"
+
+type Notifier interface {
+	Notify(message string) error
+}
+
+type NotifyService struct {
+	Services []Notifier
+}
+
+func NewNotifyService(services ...Notifier) NotifyService {
+	return NotifyService{
+		Services: services,
+	}
+}
+
+func (n NotifyService) Notify(message string) {
+	for _, notifier := range n.Services {
+		err := notifier.Notify(message)
+		if err != nil {
+			// handle error
+			log.Println(err)
+		}
+	}
+}
